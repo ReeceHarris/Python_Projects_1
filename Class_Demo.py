1ebfa0fa6bd938cde247171ad5d4d036e6942466
Reece <ReeceHarris423@gmail.com>
2020-12-17 02:05:16 -0700

    Added Comments

--- Class_Demo.py
+++ Class_Demo.py
@@ -1,22 +1,21 @@
 
-def __init__(Soldier, name, rank, ID):
-    Soldier.name = name
+def __init__(Soldier, name, rank, ID): #Defines new object
+    Soldier.name = name  #Initial Attributes 
     Soldier.rank = rank
     Soldier.ID = ID 
 
 
 
-
-class Soldier:
-    name = 'Smith'
+class Soldier:    # Main class being made 
+    name = 'Smith' 
     rank = 'spc'
     ID = '12345'
 
-class airborne(Soldier):
-    jumps = '12'
+class airborne(Soldier): #Child class of Soldier 
+    jumps = '12'  #Added attribute 
     qualified = True
 
 
-class MOS(Soldier):
-    MOSQ = True
+class MOS(Soldier):  # Chile od class Soldier 
+    MOSQ = True  #Added Attribute 
     Job = '11B' 
