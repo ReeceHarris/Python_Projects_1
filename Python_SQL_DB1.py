d64491267ff4bc35402ec0c5caf85ce26a2ab855
Reece <ReeceHarris423@gmail.com>
2020-12-17 01:31:03 -0700

    Added assignments

---
+++ Python_SQL_DB1.py
@@ -0,0 +1,36 @@
+
+ 
+import sqlite3   # Imports sqlite3 for additional functionality 
+
+connect = sqlite3.connect('test2.db')
+
+with connect:
+    A = connect.cursor() # creates new DB with 2 fields 1 integer 1 string
+    A.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+        File_list TEXT)") #Creates tbl_persons with 2 columns 1 ID 1 Text 
+    connect.commit()
+connect.close() #Disconnect from test2.db 
+
+connect = sqlite3.connect('test2.db') # Connects to test2.db
+
+with connect:
+    A = connect.cursor() #Connecting with DB and adding strings
+
+fileList = ('information.docx', 'Hello.txt', 'myImage.png', \
+            'myMovie.mpg', 'World.txt', 'data.pdf','myPhoto.jpg') #Tuple of values
+
+for x in fileList:
+    if x.endswith('.txt'): #Condition for file filtering
+        with connect: # While loop for searching
+            A = connect.cursor()
+            A.execute("INSERT INTO tbl_persons (File_list) VALUES (?)", (x,))
+            print(x) # Print results 
+
+
+connect.close()  # Disconnect from test2.db
+
+
+
+
+
