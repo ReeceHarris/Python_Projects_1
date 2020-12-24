6133e6c86aff2da7ea88d6e4ce72ecd82c34760f
Reece <ReeceHarris423@gmail.com>
2020-12-24 00:41:08 -0700

    Abstract Method Example

---
+++ abstract_method.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod  # importing abstract method 
+
+class cars(ABC): # defining parent class cars 
+
+   def movement(self): # defining parent class
+      pass  
+
+class Truck():
+
+   def movement(self): 
+      print("A truck goes slow.")
+
+class RaceCar():
+   
+   def movement(self):
+      print("Race cars go fast!")
+
+
+T = Truck()  # Calling the classes 
+T.movement()
+
+R = RaceCar()
+R.movement()
+
