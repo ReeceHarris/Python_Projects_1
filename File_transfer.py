358d516d8db6bfc4bab390c7ec51be0fc255888a
Reece <ReeceHarris423@gmail.com>
2020-12-31 21:08:56 -0700

    added comments

---
+++ File_transfer.py
@@ -0,0 +1,5 @@
+import shutil
+import os 
+
+#set where the source of the files are 
+source = '/Users/Reece/Desktop/Folder_A/'
--- python_website_opener.py
+++ python_website_opener.py
@@ -1,30 +1,30 @@
-import webbrowser 
-from tkinter import *
+import webbrowser    #Import webbrowser module for more functionality 
+from tkinter import * #import Tkinter for gui creation and costumization 
 
-window = Tk() 
+window = Tk() # sets value for window to Tkinter variable 
 windowFrame = Frame(window)
-windowFrame.pack() 
-window.title("Webpage Generator")
+windowFrame.pack() # Forces all buttons and windows to be packed together 
+window.title("Webpage Generator") # title for the program 
 label = Label(windowFrame, anchor = CENTER, text = "This is more content V2") 
-label.configure(font = ("helvetica", 14))
-label.grid(column = 0, row = 0, pady = 14, padx = 12, sticky = N+W) 
-txt = Text(windowFrame, wrap = WORD, width = 30, height = 12)
+label.configure(font = ("helvetica", 14)) # styling for label 
+label.grid(column = 0, row = 0, pady = 14, padx = 12, sticky = N+W) # styling and positioning for label
+txt = Text(windowFrame, wrap = WORD, width = 30, height = 12) 
 txt.grid(column = 0, row = 1, padx = 20, pady =20)
 
-def clicked(): 
+def clicked(): # function clicked will be called when button click to create page is clicked
    usercontent = txt.get('1.0', END)
    
-   def webPage(content): 
+   def webPage(content): # Function webPage creates the basic website 
       f = open("example_site.html", "w")
-      f.write("<html><body><h2>{}</h2></body></html>".format(content))
+      f.write("<html><body><h2>{}</h2></body></html>".format(content)) #format method inserts the added string inputed by the user 
       f.close()
-      webbrowser.open("example_site.html")
-   webPage(usercontent)
+      webbrowser.open("example_site.html") # Opens the website into the web browser 
+   webPage(usercontent) # calls webPage from within function clicked 
 
-button = Button(windowFrame, anchor = CENTER, text = "Click to create page", command = clicked)
-button.grid(row = 2, column = 3, padx = 5, pady = 5, sticky = N)
+button = Button(windowFrame, anchor = CENTER, text = "Click to create page", command = clicked) # button to call function webPage 
+button.grid(row = 2, column = 3, padx = 5, pady = 5, sticky = N) # button styling 
 
 
 
-window.mainloop()
+window.mainloop()  # Keeps the gui open till dismissed 
 
